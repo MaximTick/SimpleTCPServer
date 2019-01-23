ac957492b2b40df0cfe449a13c9c1a9040c48cae
Maxim <maxim.tickhonovich@gmail.com>
2019-01-23 12:33:48 +0300

    serv

--- Server/Server/Form1.Designer.cs
+++ Server/Server/Form1.Designer.cs
@@ -28,13 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.AcceptConnection = new System.Windows.Forms.Button();
+            this.ReceiveImage = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(51, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(592, 354);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // AcceptConnection
+            // 
+            this.AcceptConnection.Location = new System.Drawing.Point(51, 373);
+            this.AcceptConnection.Name = "AcceptConnection";
+            this.AcceptConnection.Size = new System.Drawing.Size(130, 65);
+            this.AcceptConnection.TabIndex = 2;
+            this.AcceptConnection.Text = "Accept connection";
+            this.AcceptConnection.UseVisualStyleBackColor = true;
+            this.AcceptConnection.Click += new System.EventHandler(this.AcceptConnection_Click);
+            // 
+            // ReceiveImage
+            // 
+            this.ReceiveImage.Location = new System.Drawing.Point(513, 382);
+            this.ReceiveImage.Name = "ReceiveImage";
+            this.ReceiveImage.Size = new System.Drawing.Size(130, 65);
+            this.ReceiveImage.TabIndex = 3;
+            this.ReceiveImage.Text = "Receive image";
+            this.ReceiveImage.UseVisualStyleBackColor = true;
+            this.ReceiveImage.Click += new System.EventHandler(this.ReceiveImage_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ReceiveImage);
+            this.Controls.Add(this.AcceptConnection);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Button AcceptConnection;
+        private System.Windows.Forms.Button ReceiveImage;
     }
 }
 
--- Server/Server/Form1.cs
+++ Server/Server/Form1.cs
@@ -1,20 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Server
 {
     public partial class Form1 : Form
     {
+        Socket client;
+        Socket socket;
+        byte[] buffer;
+
+        MemoryStream stream;
+
         public Form1()
         {
             InitializeComponent();
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            buffer = new byte[1000000];
+            stream = new MemoryStream(buffer);
+        }
+
+        private async void AcceptConnection_Click(object sender, EventArgs e)
+        {
+            client = await socket.AcceptAsync();
+            MessageBox.Show("New connection");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            socket.Bind(new IPEndPoint(IPAddress.Any, 3000));
+            socket.Listen(5);
+        }
+
+        private void ReceiveImage_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                client.Receive(buffer);
+                pictureBox.Image = Image.FromStream(stream);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Сan't get a picture");
+            }
         }
     }
 }
